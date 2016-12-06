2fc53570a5e3d2e05360425ed63254e91c09a5ab
nicomahery <nicomahery@gmail.com>
2016-12-06 20:15:11 +0100

    First commit: Creation of the Spring boot server which can connect to the database and create table

---
+++ ProjetGL/src/main/java/fr/descartes/miage/gl/project/ProjetGlApplication.java
@@ -0,0 +1,12 @@
+package fr.descartes.miage.gl.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetGlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetGlApplication.class, args);
+	}
+}
---
+++ ProjetGL/src/main/java/fr/descartes/miage/gl/project/entities/User.java
@@ -0,0 +1,61 @@
+package fr.descartes.miage.gl.project.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable{
+	
+	@Id @GeneratedValue
+	private long id;
+	private String username;
+	private String firstName;
+	private String lastName;
+	private String mailAddress;
+	private boolean gender;
+	
+	public User() {
+		super();
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getMailAddress() {
+		return mailAddress;
+	}
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+	public boolean isGender() {
+		return gender;
+	}
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
+	
+	
+}
