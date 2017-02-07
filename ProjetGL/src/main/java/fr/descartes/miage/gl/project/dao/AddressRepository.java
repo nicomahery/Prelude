d8d5ba1c3daa5b30b120c7346480e1f55cadc391
wei he <hewei713@gmail.com>
2017-02-07 17:29:50 +0100

    begin research by stage function

--- ProjetGL/src/main/java/fr/descartes/miage/gl/project/dao/AddressRepository.java
+++ ProjetGL/src/main/java/fr/descartes/miage/gl/project/dao/AddressRepository.java
@@ -1,9 +1,13 @@
 package fr.descartes.miage.gl.project.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import fr.descartes.miage.gl.project.entities.Address;
+import fr.descartes.miage.gl.project.entities.Advertisement;
+import fr.descartes.miage.gl.project.entities.User;
 
 public interface AddressRepository extends JpaRepository<Address, Long>{
-	
+	public List<Advertisement> findByState(String state);
 }
